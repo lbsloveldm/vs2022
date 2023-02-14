c8973028438c6d94b0337bb7e188a230c7e2f74c
Li Alex <35375980@qq.com>
2023-02-14 19:56:54 +0800

    添加mylib 类，但是编译错误，不能识别stdio.h  和 iostream引入的函数。

---
+++ libtest/StaticLib/MyLib.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "pch.h"
+#include "MyLib.h"
+
+
+void MyLib::testInLib()
+{
+	std::cout<<"¾²Ì¬¿â¼ÓÔØ£ºin library\n"<< std::endl;
+	printf("ssss");
+}
+
---
+++ libtest/StaticLib/MyLib.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class MyLib
+{
+public:
+	void testInLib();
+
+};
+
