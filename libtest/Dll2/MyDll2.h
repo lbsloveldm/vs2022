38480f2e28a8c536d2c4d7066fbf83a26cc2684d
Li Alex <35375980@qq.com>
2023-02-15 11:22:50 +0800

    添加函数声明标准

--- libtest/Dll2/MyDll2.h
+++ libtest/Dll2/MyDll2.h
@@ -4,5 +4,24 @@
 #include <windows.h>
 
 
+#define __DLL_EXPORTS__
+#ifdef __DLL_EXPORTS__
+#define DLLAPI  __declspec(dllexport)
+#else
+#define DLLAPI __declspec(dllimport)
+#endif
+
+/*
+*   https://learn.microsoft.com/en-us/windows/win32/dlls/creating-a-simple-dynamic-link-library
+*/
+
+#ifdef __cplusplus    // If used by C++ code, 
+extern "C" {          // we need to export the C interface
+#endif
+
+
 __declspec(dllexport) void __cdecl  dll2Func();
 
+#ifdef __cplusplus
+}
+#endif
